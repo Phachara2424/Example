2994e8189cb632e02240dc4bed79ada6d0ef6bbf
TBKUNGZ <PHACHARA@TBKUNGZ-PC>
2026-01-21 00:21:38 +0700

    Second Commit

---
+++ Phachara/src/hello.java
@@ -0,0 +1,37 @@
+
+public class hello {
+
+	
+
+	
+	public static void main(String[] args) {
+		
+		//INPUT 
+		int a = 100;
+		int b = 20;
+		int r = 200;
+		
+		
+        //PROCESS
+    	
+    	int c = a+b;
+    	int d = a-b;
+    	int e = a*b;
+    	int f = a/b;
+    	int t = r+b;
+    	
+    	
+    	
+    	//OUTPUT
+    	System.out.println(a+ "+" +b + " = "  +c);
+    	System.out.println(a+ "-" +b + " = " +d);
+    	System.out.println(a+ "*" +b + " = "  +e);
+    	System.out.println(a+ "/" +b + " = "  +f);
+    	System.out.println(r+ "+" +b + " = "  +t);
+    	
+		
+		
+
+	}
+
+}
