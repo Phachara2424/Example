f019ed925af323a3db4c7d27860e81eed3d618de
Phachara2424 <Funny.654@hotmail.com>
2026-01-21 00:52:32 +0700

    OK

--- src/java/ExampleS.java
+++ src/java/ExampleS.java
@@ -1,8 +1,33 @@
-package java;
 
 public class ExampleS {
 
-	System.out.println("Hello");
+	
+
+	
+	public static void main(String[] args) {
+		
+		//INPUT 
+		int a = 100;
+		int b = 20;
+		int g = 100;
+		
+        //PROCESS
+    	
+    	int c = a+b;
+    	int d = a-b;
+    	int e = a*b;
+    	int f = a/b;
+    	int h = g-b+a;
+    	
+    	
+    	//OUTPUT
+    	System.out.println(a+ "+" +b + " = "  +c);
+    	System.out.println(a+ "-" +b + " = " +d);
+    	System.out.println(a+ "*" +b + " = "  +e);
+    	System.out.println(a+ "/" +b + " = "  +f);
+    	System.out.println(g+ "-" +b + "+" + a +   " = "  +h);
+		
+		
 
 	}
 
